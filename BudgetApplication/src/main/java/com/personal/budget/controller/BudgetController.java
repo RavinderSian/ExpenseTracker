4f7eac462275442bdbfdca6cddc3ed6039d4d2fe
ravinder sian <rsian761@gmail.com>
2022-10-18 20:58:57 +0100

    add to model

--- BudgetApplication/src/main/java/com/personal/budget/controller/BudgetController.java
+++ BudgetApplication/src/main/java/com/personal/budget/controller/BudgetController.java
@@ -1,13 +1,23 @@
 package com.personal.budget.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.personal.budget.service.ExpenseService;
 
 @Controller
 public class BudgetController {
 	
+	@Autowired
+	private ExpenseService service;
+	
 	@RequestMapping("/budget")
-	public String budget() {
+	public String budget(Model model) {
+		
+		model.addAttribute("expenses", service.findAll());
+		
 		return "budget";
 	}
 
