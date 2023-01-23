4933da99235930497efc59ecae96fe5a06b04e78
ravinder sian <rsian761@gmail.com>
2023-01-23 21:52:19 +0000

    set up analysis controller

--- BudgetApplication/src/main/java/com/personal/budget/controller/AnalysisController.java
+++ BudgetApplication/src/main/java/com/personal/budget/controller/AnalysisController.java
@@ -1,13 +1,52 @@
 package com.personal.budget.controller;
 
+import java.time.LocalDate;
+import java.util.ArrayList;
+
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.personal.budget.model.Expense;
+import com.personal.budget.model.ExpenseDTO;
+import com.personal.budget.model.User;
+import com.personal.budget.service.ExpenseService;
+import com.personal.budget.service.UserService;
 
 @Controller
 public class AnalysisController {
+	
+	private final ExpenseService service;
+	private final UserService userService;
+	
+	public AnalysisController(ExpenseService service, UserService userService) {
+		this.service = service;
+		this.userService = userService;
+	}
 
 	@GetMapping("/analysis")
-	public String getGraph() {
+	public String getGraph(Model model, HttpServletRequest request) {
+		
+		if (request.getUserPrincipal() == null) {
+			model.addAttribute("expenses", new ArrayList<>());
+		} else {
+			Long userId = userService.findByUsername(request.getUserPrincipal().getName()).get().getId();
+			model.addAttribute("expenses", service.findExpensesByYearForUser(LocalDate.now().getYear(), userId));
+		}
+		
+		model.addAttribute("currentYear", LocalDate.now().getYear());
+		model.addAttribute("previousYear", LocalDate.now().getYear() - 1);
+		model.addAttribute("nextYear", LocalDate.now().getYear() + 1);
+		
+		model.addAttribute("currentMonth", LocalDate.now().getMonth().toString());
+		
+		model.addAttribute("expense", new Expense());
+		model.addAttribute("expenseToEdit", new ExpenseDTO());
+
+
+		model.addAttribute("user", new User());
 		return "line-chart";
 	}
 }
