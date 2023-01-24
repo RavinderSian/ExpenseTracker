26fef96c0e62e3b845fa6c7dbf93048682b0eb16
ravinder sian <rsian761@gmail.com>
2023-01-24 07:23:30 +0000

    get graph to show expenses

--- BudgetApplication/src/main/resources/static/js/lineChart.js
+++ BudgetApplication/src/main/resources/static/js/lineChart.js
@@ -1,24 +1,26 @@
+'use strict';
+
+const dataArray = [];
+console.log(expenses);
+
+const dataReal = expenses.forEach(expense => dataArray.push({date: expense.purchaseDate, amount: expense.amount}));
+console.log(dataArray);
+
+
+
 (async function() {
-  const data = [
-    { year: 2010, count: 10 },
-    { year: 2011, count: 20 },
-    { year: 2012, count: 15 },
-    { year: 2013, count: 25 },
-    { year: 2014, count: 22 },
-    { year: 2015, count: 30 },
-    { year: 2016, count: 28 },
-  ];
+  const data = dataArray;
 
   new Chart(
     document.getElementById('acquisitions'),
     {
       type: 'line',
       data: {
-        labels: data.map(row => row.year),
+        labels: data.map(row => row.date),
         datasets: [
           {
             label: 'Acquisitions by year',
-            data: data.map(row => row.count)
+            data: data.map(row => row.amount)
           }
         ]
       }
