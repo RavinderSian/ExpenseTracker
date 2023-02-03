5283f1a1219e8da8038af30b7f13f95fa59a4d25
ravinder sian <rsian761@gmail.com>
2023-02-03 20:35:03 +0000

    Add expense tests for budget page

---
+++ BudgetApplication/src/test/java/com/personal/budget/controller/ExpenseControllerTest.java
@@ -0,0 +1,126 @@
+package com.personal.budget.controller;
+
+import static org.hamcrest.CoreMatchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.model;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
+
+import java.math.BigDecimal;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.Optional;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.context.annotation.Import;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import com.personal.budget.config.SecurityConfig;
+import com.personal.budget.model.Expense;
+import com.personal.budget.model.ExpenseDTO;
+import com.personal.budget.model.User;
+import com.personal.budget.repository.ExpenseRepository;
+import com.personal.budget.repository.UserRepository;
+import com.personal.budget.service.ExpenseService;
+import com.personal.budget.service.ExpenseServiceImpl;
+import com.personal.budget.service.UserService;
+import com.personal.budget.service.UserServiceImpl;
+
+@WebMvcTest(ExpenseController.class)
+@Import(SecurityConfig.class)
+class ExpenseControllerTest {
+	
+	@Autowired
+	private MockMvc mockMvc;
+	
+	private ExpenseController controller;
+	
+	@MockBean
+	private ExpenseService service;
+	
+	@MockBean
+	private ExpenseRepository expenseRepository;
+	
+	@MockBean
+	private UserService userService;
+	
+	@MockBean
+	private UserRepository userRepository;
+	
+	@BeforeEach
+	void setUp() throws Exception {
+		controller = new ExpenseController(new ExpenseServiceImpl(expenseRepository), new UserServiceImpl(userRepository));
+	}
+	
+	@Test
+	void test_Budget_DisplaysPageAsExpected_WhenHitWithNoLogin() throws Exception {
+		
+		mockMvc.perform(get("/budget"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("budget"))
+		.andExpect(model().attribute("expenses", hasSize(0)))
+		.andExpect(model().attribute("currentYear", equalTo(LocalDate.now().getYear())))
+		.andExpect(model().attribute("previousYear", equalTo(LocalDate.now().getYear()-1)))
+		.andExpect(model().attribute("nextYear", equalTo(LocalDate.now().getYear()+1)))
+		.andExpect(model().attribute("currentMonth", LocalDate.now().getMonth().toString()))
+		.andExpect(model().attribute("expense", equalTo(new Expense())))
+		.andExpect(model().attribute("expenseToEdit", equalTo(new ExpenseDTO())))
+		.andExpect(model().attribute("user", equalTo(new User())));
+		
+	}
+	
+	@Test
+	@WithMockUser(username = "rsian", password = "pw", authorities = "USER")
+	void test_Budget_DisplaysPageAsExpected_WhenHitWithLoggedInUser() throws Exception {
+		
+		Expense expense = new Expense();
+		expense.setUserId(1L);
+		expense.setAmount(BigDecimal.valueOf(10));
+		expense.setCategory("Dates");
+		expense.setDescription("car");
+		expense.setPurchaseDate(LocalDate.now());
+		
+		Expense expense2 = new Expense();
+		expense2.setUserId(1L);
+		expense2.setAmount(BigDecimal.valueOf(10));
+		expense2.setCategory("Dates");
+		expense2.setDescription("cars");
+		expense2.setPurchaseDate(LocalDate.now());
+		
+		User user = new User();
+		user.setId(1L);
+		user.setAuthority("USER");
+		user.setEmail("rsian@gmail.com");
+		user.setPassword("test");
+		user.setUsername("testing");
+		
+		when(userService.findByUsername("rsian")).thenReturn(Optional.of(user));
+		
+		when(service.findExpensesByYearForUser(LocalDate.now().getYear(), 1L))
+			.thenReturn(Arrays.asList(expense, expense2));
+		
+		mockMvc.perform(get("/budget"))
+		.andExpect(status().isOk())
+		.andExpect(view().name("budget"))
+		.andExpect(model().attribute("expenses", hasSize(2)))
+		.andExpect(model().attribute("currentYear", equalTo(LocalDate.now().getYear())))
+		.andExpect(model().attribute("previousYear", equalTo(LocalDate.now().getYear()-1)))
+		.andExpect(model().attribute("nextYear", equalTo(LocalDate.now().getYear()+1)))
+		.andExpect(model().attribute("currentMonth", LocalDate.now().getMonth().toString()))
+		.andExpect(model().attribute("expense", equalTo(new Expense())))
+		.andExpect(model().attribute("expenseToEdit", equalTo(new ExpenseDTO())))
+		.andExpect(model().attribute("user", equalTo(new User())));
+		
+		verify(service, times(1)).findExpensesByYearForUser(LocalDate.now().getYear(), 1L);
+	}
+
+}
