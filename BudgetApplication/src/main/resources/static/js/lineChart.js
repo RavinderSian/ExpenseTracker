94ae54845aacf7e9e866ea9ae501045b999f976b
ravinder sian <rsian761@gmail.com>
2023-02-03 19:21:14 +0000

    add real data

--- BudgetApplication/src/main/resources/static/js/lineChart.js
+++ BudgetApplication/src/main/resources/static/js/lineChart.js
@@ -1,9 +1,12 @@
 'use strict';
 
 const dataArray = [];
+
+expenses.sort((a, b) => a.purchaseDate.split('-')[2] - b.purchaseDate.split('-')[2]);
+
 console.log(expenses);
 
-const dataReal = expenses.forEach(expense => dataArray.push({date: expense.purchaseDate, amount: expense.amount}));
+const dataReal = expenses.forEach(expense => dataArray.push({...expense}));
 console.log(dataArray);
 
 
@@ -12,11 +15,11 @@ console.log(dataArray);
   const data = dataArray;
 
   new Chart(
-    document.getElementById('acquisitions'),
+    document.querySelector('.expenses-jan'),
     {
       type: 'line',
       data: {
-        labels: data.map(row => row.date),
+        labels: data.map(row => row.purchaseDate),
         datasets: [
           {
             label: 'Acquisitions by year',
