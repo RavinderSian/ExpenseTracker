1893bf8d6677b5e0d48f48c210673aedf23a1929
ravinder sian <rsian761@gmail.com>
2023-02-18 10:30:54 +0000

    Add s3 dependency

--- BudgetApplication/src/main/java/com/personal/budget/export/CsvExport.java
+++ BudgetApplication/src/main/java/com/personal/budget/export/CsvExport.java
@@ -27,45 +27,50 @@ public class CsvExport {
 
 
 	public void generateCSV(Long userId) throws IOException {
-
-        // name of generated csv
-        final String CSV_LOCATION = "Expenses.csv ";
-  
-        try {
-  
-            // Creating writer class to generate
-            // csv file
-            FileWriter writer = new FileWriter(CSV_LOCATION);
-  
-            // create a list of employee
-            List<Expense> expenseList = service.findByUserId(userId);
-  
-            // Create Mapping Strategy to arrange the 
-            // column name in order
-            HeaderColumnNameMappingStrategy<Expense> mappingStrategy = new HeaderColumnNameMappingStrategy<>();
-           
-            mappingStrategy.setType(Expense.class);
-  
-//            // Arrange column name as provided in below array.
-//            String[] columns = new String[] 
-//                    { "id", "userId", "category", "amount", "description", "purchaseDate" };
-//            mappingStrategy.setco
+//
+//        // name of generated csv
+//        final String CSV_LOCATION = "Expenses.csv ";
 //  
-            // Creating StatefulBeanToCsv object
-            StatefulBeanToCsvBuilder<Expense> builder = new StatefulBeanToCsvBuilder<>(writer);
-            
-            StatefulBeanToCsv<Expense> beanWriter = builder.withMappingStrategy(mappingStrategy).build();
-  
-            // Write list to StatefulBeanToCsv object
-            beanWriter.write(expenseList);
-  
-            // closing the writer object
-            writer.close();
-        }
-        catch (Exception e) {
-            log.error(e.getMessage(), e);
-        }
+//        try {
+//  
+//            // Creating writer class to generate
+//            // csv file
+//            FileWriter writer = new FileWriter(CSV_LOCATION);
+//  
+//            // create a list of employee
+//            List<Expense> expenseList = service.findByUserId(userId);
+//  
+//            // Create Mapping Strategy to arrange the 
+//            // column name in order
+//            HeaderColumnNameMappingStrategy<Expense> mappingStrategy = new HeaderColumnNameMappingStrategy<>();
+//           
+//            mappingStrategy.setType(Expense.class);
+//  
+////            // Arrange column name as provided in below array.
+////            String[] columns = new String[] 
+////                    { "id", "userId", "category", "amount", "description", "purchaseDate" };
+////            mappingStrategy.setco
+////  
+//            // Creating StatefulBeanToCsv object
+//            StatefulBeanToCsvBuilder<Expense> builder = new StatefulBeanToCsvBuilder<>(writer);
+//            
+//            StatefulBeanToCsv<Expense> beanWriter = builder.withMappingStrategy(mappingStrategy).build();
+//  
+//            // Write list to StatefulBeanToCsv object
+//            beanWriter.write(expenseList);
+//  
+//            // closing the writer object
+//            writer.close();
+//        }
+//        catch (Exception e) {
+//            log.error(e.getMessage(), e);
+//        }
     }
+	
+	public void generateCSVAndUploadToS3(Long userId) throws IOException {
+		
+	}
+	
 
 
 }
